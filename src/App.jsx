681fd2bdb1d1408bb27eb0e6899d973b987323f2
Chris <chrisoconnell21@gmail.com>
2025-12-10 22:00:15 -0800

    0.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Navigation from './components/Navigation'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -8,9 +9,20 @@ import Contact from './pages/Contact'
 import './App.css'
 import './pages/PageLayout.css'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="container">
         <Navigation />
         <Routes>
